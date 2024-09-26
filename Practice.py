2302b8423a122f2cad3cd2c965ab85ee1bbcf077
josh3287 <91765645+josh3287@users.noreply.github.com>
2024-09-26 18:35:11 -0400

    Main

---
+++ Practice.py
@@ -0,0 +1,10 @@
+print("Welcome to the rollercoaster!")
+height = int(input("Enter your height in cm: "))
+age = int(input("How old are you? "))
+
+if height > 120:
+        print("You are tall enough to enter, go ahead and have fun!")
+elif age > 18:
+              print("You meet the requirements!")
+else:
+    print("Sorry, too short!")
---
+++ Practice1.py
@@ -0,0 +1,5 @@
+number_to_check = int(input("Enter a number: "))
+if number_to_check % 2 ==0:
+    print("Even")
+else:
+    print("Odd")
