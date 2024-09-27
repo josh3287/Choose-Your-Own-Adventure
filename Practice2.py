d907e719ef018ef6cad534caad69c284d2c9a9ba
josh3287 <91765645+josh3287@users.noreply.github.com>
2024-09-27 14:37:25 -0400

    Additions

--- Practice2.py
+++ Practice2.py
@@ -1,14 +1,22 @@
-print("Welcome to the rollercoaster!")
+print("Welcome to the rollercoaster! Height and age are required for price")
 height = int(input("Enter your height in cm: "))
+bill = 0
 
 if height >= 120:
         print("You are tall enough!")
         age = int(input("How old are you? "))
         if age <= 12:
-                print("Please pay $5")
+                bill = 5
+                print("Child tickets are $5")
         elif age <= 18:
-                print("Please pay $7")
+                bill = 7
+                print("Youth tickets are $7")
         else:
-                print("Please pay $11")
+                bill = 11
+                print("Adult tickets are $11")
+        wants_photo = input("Would you like a photo? Y or N")
+        if wants_photo == "y":
+                bill  +=3
+        print(f"Your total is {bill}")
 else:
         print("You are not tall enough to ride on your own and must be accompanied by an adult!")
